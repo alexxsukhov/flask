8990c0da725b7d8458594209dfc3582e13103d71
AlexxS <alexx.fr-man@yandex.ru>
2023-08-28 23:23:32 +0300

    add feature for generate product and categories

--- app.py
+++ app.py
@@ -2,6 +2,46 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+categories = [
+    {
+        "id": 1,
+        "name": "Обувь",
+        "announce": "Обувь для всей семьи",
+        "slug": "shoes",
+        "img_src": "/static/images/shoes.jpg",
+    },
+    {
+        "id": 2,
+        "name": "Одежда",
+        "slug": "cloth",
+        "announce": "Одежда для всей семьи",
+        "img_src": "/static/images/cloth.jpg",
+    },
+]
+
+products = [
+    {
+        "id": 1,
+        "category_id": 1,
+        "name": "Кеды",
+        "announce": "Кеды для всей семьи",
+        "slug": "shoes",
+        "price": "5000",
+        "img_src": "/static/images/shoes.jpg",
+        "description": "Отличные кеды, подойдут любому, только выберите размер",
+    },
+    {
+        "id": 2,
+        "category_id": 2,
+        "name": "Куртка",
+        "announce": "Великолепие",
+        "slug": "jacket",
+        "price": "5000",
+        "img_src": "/static/images/jacket.webp",
+        "description": "Хорошая куртка",
+    },
+]
+
 
 @app.route('/')
 def index():  # put application's code here
@@ -11,105 +51,45 @@ def index():  # put application's code here
 @app.route('/catalog/')
 def catalog():  # put application's code here
     context = {
-        "title": "Каталог одежды и обуви",
-        "categories": [
-            {
-                "name": "Обувь",
-                "slug": "shoes",
-                "img": "/static/images/shoes.jpg",
-            },
-            {
-                "name": "Одежда",
-                "slug": "cloth",
-                "img": "/static/images/vidy-kurtok.jpg",
-            }
-        ],
+        "categories": categories,
     }
 
     return render_template("catalog.html", **context)
 
 
-@app.route('/catalog/shoes/')
-def category_shoes():  # put application's code here
-    context = {
-        "title": "Обувь",
-        "products": [
-            {
-                "name": "Кеды",
-                "slug": "sneakers",
-                "img": "/static/images/shoes.jpg",
-            },
-            {
-                "name": "Кеды",
-                "slug": "sneakers",
-                "img": "/static/images/shoes.jpg",
-            },
-            {
-                "name": "Кеды",
-                "slug": "sneakers",
-                "img": "/static/images/shoes.jpg",
-            },
-        ],
-    }
+@app.route('/catalog/<slug_category>/')
+def page_category(slug_category):  # put application's code here
+    for category in categories:
 
-    return render_template("category.html", **context)
+        if category["slug"] == slug_category:
+            products_list = []
+            for product in products:
+                if product["category_id"] == category["id"]:
+                    products_list.append(product)
+            context = {
+                "category": category,
+                "products": products_list,
+            }
+            return render_template("category.html", **context)
+    else:
+        return render_template("404.html")
 
 
-@app.route('/catalog/cloth/')
-def category_cloth():  # put application's code here
-    context = {
-        "title": "Каталог одежды и обуви",
-        "products": [
-            {
-                "name": "Куртка мужская",
-                "slug": "jacket",
-                "img": "/static/images/jacket.webp",
-            },
-            {
-                "name": "Куртка мужская",
-                "slug": "jacket",
-                "img": "/static/images/jacket.webp",
-            },
-            {
-                "name": "Куртка мужская",
-                "slug": "jacket",
-                "img": "/static/images/jacket.webp",
-            },
-        ],
-    }
+@app.route('/product/<slug_product>/')
+def page_product(slug_product):  # put application's code here
+    for product in products:
+        if product["slug"] == slug_product:
+            context = {
+                "name": product["name"],
+                "price": product["price"],
+                "img": product["img_src"],
+                "description": product["description"],
+            }
 
-    return render_template("category.html", **context)
-
-
-@app.route('/product/jacket/')
-def product_jacket():  # put application's code here
-    context = {
-        "title": "Куртка мужская",
-        "name": "Куртка мужская",
-        "slug": "jacket",
-        "img": "/static/images/jacket.webp",
-        "price": "10000",
-        "count": "100",
-        "description": "Куртка классная и прекрасная, покупай! Слякоть ни по чем, дождь остается в стороне!",
-    }
-
-    return render_template("product.html", **context)
-
-
-@app.route('/product/sneakers/')
-def product_sneakers():  # put application's code here
-    context = {
-        "title": "Кеды",
-        "name": "Кеды",
-        "slug": "jacket",
-        "img": "/static/images/shoes.jpg",
-        "price": "5000",
-        "count": "100",
-        "description": "Кеды классные и прекрасные, покупай! Слякоть ни по чем, дождь остается в стороне!"
-    }
-
-    return render_template("product.html", **context)
+            return render_template("product.html", **context)
+    else:
+        return render_template("404.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
